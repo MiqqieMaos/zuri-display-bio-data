4293d0765b0a90213202184020d1af4ca9d3b555
Oladipo Emmanuel Olalekan <miqqiemaos@gmail.com>
2022-06-23 17:21:35 +0100

    basic bio-data alert page
    zuri-training-task

---
+++ app.js
@@ -0,0 +1,9 @@
+const bio = {
+  myName: "OLADIPO OLALEKAN EMMANUEL",
+  myHeight: "5'11",
+  myCountry: "NIGERIA",
+};
+const { myName, myHeight, myCountry } = bio;
+alert(`My name is ${myName}.`);
+alert(`I'm ${myHeight} tall.`);
+alert(`I'm from ${myCountry}.`);
